c7987d83d744f34c979604edf6d05be6b2fbc412
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2021-12-02 13:06:21 +0100

    Revert "Revert "Revert "Log alle tema, for test"""
    This reverts commit 6542f915424fc444185e1de20d77c9df06141ddf.

--- src/main/kotlin/no/nav/tpts/joark/mottak/JournalfoeringLogger.kt
+++ src/main/kotlin/no/nav/tpts/joark/mottak/JournalfoeringLogger.kt
@@ -123,7 +123,7 @@ internal class JournalfoeringReplicator(
         try {
             records.onEach { record ->
                 val tema = record.value().get("temaNytt")?.toString() ?: ""
-                LOGGER.info { "$currentPositions: Mottok tema '$tema'. $record" }
+                LOGGER.info { "$currentPositions: Mottok tema '$tema'. " + if (tema == "IND" || tema == "TIL") "$record" else "Hopp over" }
                 currentPositions[TopicPartition(record.topic(), record.partition())] = record.offset() + 1
             }
         } catch (err: Exception) {
